7b338f74d18da9790457443d30bc706475788b50
TMN <t.martin@adsoftware.fr>
2024-04-28 09:30:57 +0200

    tmp tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,56 @@
-import React, { useState, useTransition } from "react";
+import { useTransition } from "react";
+import "./index.css";
 
-// Define the type for items array
-type ItemArray = string[];
-
-function App() {
-  // Sample data
-  const items: ItemArray = Array.from(
-    { length: 10000 },
-    (_, index) => `Item ${index + 1}`
-  );
-
-  // State for the search term
-  const [query, setQuery] = useState<string>("");
-
-  // State for the filtered items
-  const [filteredItems, setFilteredItems] = useState<ItemArray>(items);
-
+const App = () => {
   const [isPending, startTransition] = useTransition();
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.target;
-    setQuery(value);
-
-    // Update filtered items based on the query
-    startTransition(() => {
-      setFilteredItems(items.filter((item) => item.includes(value)));
-    });
+  const handleSubmit = async (formData: FormData) => {
+    const name = formData.get("name");
+    setTimeout(() => {
+      startTransition(() => {
+        console.log("Name updated to: ", name);
+      });
+    }, 1000);
   };
 
   return (
-    <div className="p-5">
-      <input
-        type="text"
-        value={query}
-        onChange={handleChange}
-        placeholder="Filter items..."
-        className="w-full p-2 border border-gray-300 rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-      />
-      {isPending ? (
-        <p className="text-center text-gray-500">Loading...</p>
-      ) : null}
+    <div className="max-w-md mx-auto">
+      <form>
+        <div className="space-y-12">
+          <div className=" pb-12">
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="first-name"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  First name
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    name="first-name"
+                    id="first-name"
+                    autoComplete="given-name"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
-      <ul className="max-h-96 overflow-y-scroll mt-4">
-        {filteredItems.map((item, index) => (
-          <li key={index} className="py-1">
-            {item}
-          </li>
-        ))}
-      </ul>
+        <div className="mt-6 flex items-center justify-end gap-x-6">
+          <button
+            type="submit"
+            className="rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+          >
+            Save
+          </button>
+        </div>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
